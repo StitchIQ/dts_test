c954efb68ef26ae980b039d9d3321bf8b4924730
lixing0303 <lixing0303@163.com>
2016-07-08 18:23:43 +0800

    修改了页面导航 优化了用户检查
    Signed-off-by: lixing0303 <lixing0303@163.com>

--- app/static/check_user.js
+++ app/static/check_user.js
@@ -1,19 +1,21 @@
 $(function(){
    $("#bug_owner_id").blur(function(){
-        $.get("/check_user",{
-            username:$("#bug_owner_id").val()
-        },function(data,status){
-            if(data == 'Not Found'){
-                //$("#bug_owner_id").append("<p>This field is required.</p>");
-                $("#bug_owner_id").parent().css({"color":"red","border":"2px solid red"})
-                //alert($("#bug_owner_id").html());
-            }
-            else
-            {
-                //alert($("#bug_owner_id").html());
-                $("#bug_owner_id").parent().css({"color":"#555555","border":"0px solid #cccccc"});
-            }
-        });
+        if ($("#bug_owner_id").val().length > 6){
+            $.get("/check_user",{
+                username:$("#bug_owner_id").val()
+            },function(data,status){
+                if(data == 'Not Found'){
+                    //$("#bug_owner_id").append("<p>This field is required.</p>");
+                    $("#bug_owner_id").parent().css({"color":"red","border":"2px solid red"})
+                    //alert($("#bug_owner_id").html());
+                }
+                else
+                {
+                    //alert($("#bug_owner_id").html());
+                    $("#bug_owner_id").parent().css({"color":"#555555","border":"0px solid #cccccc"});
+                }
+            });
+        }
     });
 });
 
